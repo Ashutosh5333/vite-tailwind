dfaa6ef54ea90cc0f9370026b70a9a2194d2b56e
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2024-07-05 22:25:00 +0530

    setup done

---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p className='border-2 text-red-700'>hello </p>
+      </div>
+    </>
+  )
+}
+
+export default App
